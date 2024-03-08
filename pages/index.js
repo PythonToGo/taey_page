1fe1f5f0b7d036136bab5216e096b2ca72b1da53
PythonToGo <pythontogoplease@gmail.com>
2024-03-08 12:30:19 +0100

    correcting some codes because of cubelike

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/legacy/image";
 import Layout from '../components/layout';
 import Hero from '../components/home/hero';
 // import Animation from '../components/home/animation';
+import Cube from './cubehome';
 
 export default function Home() {
 
@@ -16,12 +17,38 @@ export default function Home() {
             </Head>
             <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
                 <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+
                 <Hero/>
                 
                 </div>
             </section>
-
+            {/* <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+                <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                    <Cube />
+                </div>
+            </section> */}
         </Layout>
 
     );
 }
+
+// export default function Home() {
+//     return (
+//         <Layout>
+//             <Head>
+//                 <title>Taey&apos;s World</title>
+//                 <meta name="description" content="The way we live like Hedgehog" />
+//                 <link rel="icon" href="/hedge2.ico" />
+//             </Head>
+           
+//                 {/* <Cube /> */}
+          
+//             <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+//                 <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+//                     <Hero/>
+//                 </div>
+//             </section>
+            
+//         </Layout>
+//     );
+// }
